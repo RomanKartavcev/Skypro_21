e6aa3196f543dbf912eb5ab8cee00c08eb581287
RmanKartavcev <128297080+Ramzes8@users.noreply.github.com>
2023-03-24 13:59:37 +0900

    add feat

--- main_file.py
+++ main_file.py
@@ -1,3 +1,9 @@
 from revers_text import revers_text
 
+revers_text("Hello мир!")
+from revers_text import revers_text
+
+revers_text("Hello World!")
+revers_text("Hello World!")
+revers_text("Hello World!")
 revers_text("Hello World!")
